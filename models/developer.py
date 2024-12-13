95836acdadd8e5b23cd3c655d9156cdc66af25b0
angel.chiveg <angel.chiveg@educa.jcyl.es>
2024-12-13 10:20:14 +0100

    Herencia en developer

---
+++ models/developer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class developer(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    # Relaciones:
+    technologies = fields.Many2many('manage.technology', string='Technologies',
+                                    relation = 'developer_technologies',
+                                    column1='developer_id',
+                                    column2='technologies_id')
