48f8d9e8ba64a6de14648bf57987c62d3bb7343d
Angel Chicote <angel.chiveg@educa.jcyl.es>
2025-01-13 22:04:26 +0100

    Se importan las tareas desde trello

--- models/task.py
+++ models/task.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
+import requests
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class task(models.Model):
@@ -18,16 +20,15 @@ class task(models.Model):
     # Relaciones:
     sprint_id = fields.Many2one('managechicote.sprint', 'Sprint', compute="_get_sprint", store=True)
     technology_ids = fields.Many2many("managechicote.technology", string="Tecnologías", ondelete="cascade",
-                                relation="task_technology",
-                                column1="technology_ids",
-                                column2="task_ids")
-    history_id = fields.Many2one('managechicote.history', 'History', ondelete='cascade', required=True)
+                                      relation="task_technology",
+                                      column1="technology_ids",
+                                      column2="task_ids")
+    history_id = fields.Many2one('managechicote.history', 'History', ondelete='cascade', required=False)
 
     # Métodos:
     def _get_code(self):
         for t in self:
             t.code = "TSK_" + str(t.id)
-
 
     @api.depends('code')
     def _get_sprint(self):
@@ -42,3 +43,34 @@ class task(models.Model):
             if not found:
                 t.sprint_id = False
 
+
+    @api.model
+    def import_trello_tasks(self):
+        """Método para importar tareas desde Trello a Odoo"""
+        trello_config = self.env['managechicote.trello.config'].search([], limit=1)
+        if not trello_config:
+            raise UserError("No se ha configurado Trello correctamente.")
+
+        # URL para obtener las tarjetas (tareas) desde Trello
+        url = f'https://api.trello.com/1/boards/{trello_config.board_id}/cards'
+        params = {
+            'key': trello_config.api_key,
+            'token': trello_config.token,
+        }
+
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
+            raise UserError("Error al obtener datos de Trello.")
+
+        trello_data = response.json()
+
+        # Crear tareas en Odoo basadas en los datos de Trello
+        for card in trello_data:
+            # Puedes personalizar cómo quieres que se creen las tareas
+            self.create({
+                'name': card.get('name'),
+                'description': card.get('desc'),
+                'start_date': card.get('dateStart'),
+                'end_date': card.get('dateEnd'),
+                'is_paused': card.get('paused', False),  # Asume que el campo `paused` existe en Trello
+            })
