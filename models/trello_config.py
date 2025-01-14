bf552c42912daffeb585ba13c37189c35619aae3
Angel Chicote <angel.chiveg@educa.jcyl.es>
2025-01-14 18:26:12 +0100

    En la vista tree de trello no se puede ni crear más registros ni eliminar el único.

--- models/trello_config.py
+++ models/trello_config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
+
 
 class TrelloConfig(models.Model):
     _name = 'managechicote.trello.config'
@@ -10,20 +11,29 @@ class TrelloConfig(models.Model):
     board_id = fields.Char(string="Board ID", required=True)
 
     @api.model
-    def create(self, vals):
-        # Si ya existe un registro, modificamos ese registro en lugar de crear uno nuevo
-        existing_record = self.search([], limit=1)
-        if existing_record:
-            existing_record.write(vals)  # Actualiza el registro existente
-            return existing_record  # Devuelve el registro actualizado
-        return super(TrelloConfig, self).create(vals)
-
-    @api.model
     def init(self):
-        # Si no existe el registro, creamos el primero
+        # Si no existe el registro, se crea el primero
         if not self.search([]):
             self.create({
                 'api_key': 'DEFAULT_API_KEY',
                 'token': 'DEFAULT_TOKEN',
                 'board_id': 'DEFAULT_BOARD_ID',
             })
+
+    @api.model
+    def create(self, vals):
+        # Evitar que se puedan crear más registros, ya que la configuración solo es una.
+        # También quito el permiso del csv
+        # Y en la vista tree se oculta el botón
+        if self.search([], limit=1):
+            raise exceptions.UserError("Solo se permite un único registro de configuración.")
+        return super(TrelloConfig, self).create(vals)
+
+    # No necesita el decorador @api.model, ya que no trabaja a nivel de modelo.
+    def unlink(self):
+        # Evitar que se borre el único registro.
+        # También quito el permiso del csv
+        # Y en la vista tree se oculta el botón
+        if len(self.search([])) <= 1:
+            raise exceptions.UserError("No puedes eliminar el único registro existente.")
+        return super(TrelloConfig, self).unlink()
